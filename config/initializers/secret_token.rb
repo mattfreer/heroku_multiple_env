3eb528dda7e0aa0137d6f7fd9ee36fb5c6e65538
Matt Freer <matt.freer@gmail.com>
2011-07-21 16:06:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuMultipleEnvGemset::Application.config.secret_token = 'edead291e1a5d1ed632309ff31276e509bf6a5eb51171037b9660151cd30a74982b427818a45361778cf164593fb352f5adc54ee094d6c5c8d34802a77b29718'
